0767437467611dce4847588f5b21778e93fb380e
Alberto Palumbo <palumbo.alb@gmail.com>
2023-05-01 18:01:46 -0400

    Update settings.py

--- mpp_api/mpp_api/settings.py
+++ mpp_api/mpp_api/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-xyu+n5jlao-m@j5y*5o2$jw(hl+976bxim5+eoz2jje2#ku+k%"
+SECRET_KEY = ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
